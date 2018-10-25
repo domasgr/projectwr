a6075c835c81ca18857688d2893dd3c9220c9847
domasgr <44139891+domasgr@users.noreply.github.com>
2018-10-25 22:13:45 +0300

    added nav tracker

---
+++ code/main.js
@@ -0,0 +1,10 @@
+var nav = document.querySelectorAll("nav ul li");
+
+for(i=0; i<nav.length; i++){
+	nav[i].addEventListener("click", function(){
+		for(i=0; i<nav.length; i++){
+			nav[i].classList.remove("active");
+		}
+		this.classList.add("active");
+	})
+}
