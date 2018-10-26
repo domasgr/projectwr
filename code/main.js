ee6b3db823822cbfe0a0e545a3c4cdc8710fef03
domasgr <44139891+domasgr@users.noreply.github.com>
2018-10-26 18:22:30 +0300

    added navbar, which now is working

--- code/main.js
+++ code/main.js
@@ -1,7 +1,7 @@
-
 // *******************navbar tracker*****************
-
 var nav = document.querySelectorAll("nav ul li");
+
+nav[3].classList.add("active"); // On the page opening, we show that we are on home
 
 for(i=0; i<nav.length; i++){
 	nav[i].addEventListener("click", function(){
