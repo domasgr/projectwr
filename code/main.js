d3a77f33907ca0696f0804d6ff0ecd279cb97ded
domasgr <44139891+domasgr@users.noreply.github.com>
2018-10-27 22:21:12 +0300

    added comments to js file

--- code/main.js
+++ code/main.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
+	//variables which is needed from multiple funcstions
 	var scrollLink = $('.scroll');
 	var titleOffset = $('header').offset().top-120;
 	
+
+	//navigation scrolling
 	scrollLink.click(function(e){
 		e.preventDefault();
 		$('body,html').animate({
 			scrollTop: $(this.hash).offset().top
 		}, 1000);
 	});
+
+
 	// Active lane switching
 	 $(window).scroll(function() {
 	 	var scrollbarLocation = $(this).scrollTop();
@@ -33,57 +38,36 @@ $(document).ready(function(){
 	 			$('li a').removeClass('active');
 	 			$('li a').eq(0).addClass('active');
 	 		}
+	});
 
-	 	
-	 		// if(scrollbarLocation <= titleOffset){
-	 		// 	$('header').fadeIn();
-	 		// }
-    // var scrollbarLocation = $(this).scrollTop();
-    
-    // scrollLink.each(function() {
-      
-    //   var sectionOffset = $(this.hash).offset().top;
-      
-    //   if ( sectionOffset <= scrollbarLocation ) {
-    //   	console.log("now");
-    //   	$('nav ul li').removeClass('active');
-    //   	// console.log($('nav ul li').removeClass('active'));
-    //     $(this).parent().addClass('active');
-    //     // console.log($(this).parent().addClass('active'));
-        
-    //   }
-    // })
-    
-  });
 
+	 // header toggle fading on exit/entry
 	 $(window).scroll(function(){
 	 var scrollbarLocation = $(this).scrollTop();
-	 // var titleOffset = $('header').offset().top-100;
+	 		
 	 		if( scrollbarLocation > titleOffset){
 	 			$('header').fadeOut(300);
 	 		} else if( scrollbarLocation < titleOffset){
 	 			$('header').fadeIn();
 	 		}
-
-
   });
 });
 
 
 
 
+// COMING SOON FUNCTION FOR SMOOTH MOUSE WHEEL
+// $(window).on('wheel', function(event){
+// 	if(event.originalEvent.deltaY < 0){
+// 		console.log("up");
+// 	} else{
+// 		event.preventDefault();
+// 		$('body,html').animate({
+// 			scrollTop: $('#second').offset().top
+// 		}, 1000);
+// 	}
+// })
 
-// *******************navbar tracker*****************
-// var nav = document.querySelectorAll("nav ul li");
 
-// nav[3].classList.add("active"); // On the page opening, we show that we are on home
 
-// for(i=0; i<nav.length; i++){
-// 	nav[i].addEventListener("click", function(){
-// 		for(i=0; i<nav.length; i++){
-// 			nav[i].classList.remove("active");
-// 		}
-// 		this.classList.add("active");
-// 	});
-// }
-//*****************************************************
+
