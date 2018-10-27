cb9b3b274e617ea91f9ca7e71986f394c5f8e13c
domasgr <44139891+domasgr@users.noreply.github.com>
2018-10-27 16:06:02 +0300

    verything works

--- code/main.js
+++ code/main.js
@@ -1,3 +1,37 @@
+$(document).ready(function(){
+	var scrollLink = $('.scroll');
+
+	scrollLink.click(function(e){
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 150);
+	});
+	// Active lane switching
+	 // $(window).scroll(function() {
+  //   var scrollbarLocation = $(this).scrollTop();
+    
+  //   scrollLink.each(function() {
+      
+  //     var sectionOffset = $(this.hash).offset().top;
+      
+  //     if ( sectionOffset <= scrollbarLocation ) {
+  //     	console.log("now");
+  //     	$('nav ul li').removeClass('active');
+  //     	// console.log($('nav ul li').removeClass('active'));
+  //       $(this).parent().addClass('active');
+  //       // console.log($(this).parent().addClass('active'));
+        
+  //     }
+  //   })
+    
+  // })
+  
+});
+
+
+
+
 // *******************navbar tracker*****************
 var nav = document.querySelectorAll("nav ul li");
 
@@ -9,6 +43,6 @@ for(i=0; i<nav.length; i++){
 			nav[i].classList.remove("active");
 		}
 		this.classList.add("active");
-	})
+	});
 }
 //*****************************************************
