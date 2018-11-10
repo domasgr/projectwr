9f88ca64ec0bb8b40c7a934b9c7ac7f92dfbe295
domasgr <44139891+domasgr@users.noreply.github.com>
2018-11-10 16:25:29 +0200

    keli css pakitimai

--- backend/login.php
+++ backend/login.php
@@ -2,7 +2,7 @@
 <?php
 if (isset($_POST['submitLogin'])) {
     include "../mysql_connect.php";
-    $username = $_POST['usernameOrEmail'];
+    $username = $_POST['username'];
     $password = $_POST['password'];
 
     if (empty($username) || empty($password)) {
@@ -30,14 +30,26 @@ if (isset($_POST['submitLogin'])) {
 }
 ?>
 <main>
-    <h1>Log in beta</h1>
-
-    <form action="/backend/login.php" method="POST">
-        <input type="text" name="usernameOrEmail">
-        <input type="text" name="password">
-        <input type="submit" name="submitLogin">
-    </form>
-
+    <div class="container login">
+        <div class="row">
+            <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <form action="/backend/login.php" method="POST">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" class="form-control" name="username" placeholder="Username">
+                            <small id="emailHelp" class="form-text text-muted">Login for web administrator</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" name="password" placeholder="Password">
+                        </div>
+                        <input type="submit" name="submitLogin" class="btn btn-primary" value="Login">
+                    </form>
+                </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
 </main>
 
 
